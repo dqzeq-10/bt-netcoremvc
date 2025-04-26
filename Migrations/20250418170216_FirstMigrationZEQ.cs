d724a094233c7dde91b1414f5cd7efddadf238a3
Zeq <124875024+ZEQ-10@users.noreply.github.com>
2025-04-26 09:16:46 +0700

    zeq

---
+++ Migrations/20250418170216_FirstMigrationZEQ.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BT_NetcoreMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigrationZEQ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DepartmentID = table.Column<int>(type: "int", nullable: false),
+                    DepartmentName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Departme__B2079BCD88E42FAF", x => x.DepartmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    DepartmentID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Employee__7AD04FF1A0D75DF3", x => x.EmployeeID);
+                    table.ForeignKey(
+                        name: "FK__Employee__Depart__398D8EEE",
+                        column: x => x.DepartmentID,
+                        principalTable: "Department",
+                        principalColumn: "DepartmentID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentID",
+                table: "Employee",
+                column: "DepartmentID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
---
+++ Models/BTNcMVCDbContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BT_NetcoreMVC.Models;
+
+public partial class BTNcMVCDbContext : DbContext
+{
+    public BTNcMVCDbContext()
+    {
+    }
+
+    public BTNcMVCDbContext(DbContextOptions<BTNcMVCDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Name=ConnectionStrings:BTNetcoreMVC");
+        }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.DepartmentId).HasName("PK__Departme__B2079BCD88E42FAF");
+
+            entity.ToTable("Department");
+
+            entity.Property(e => e.DepartmentId)
+                .ValueGeneratedNever()
+                .HasColumnName("DepartmentID");
+            entity.Property(e => e.DepartmentName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04FF1A0D75DF3");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.EmployeeId)
+                .ValueGeneratedNever()
+                .HasColumnName("EmployeeID");
+            entity.Property(e => e.Address).HasMaxLength(50);
+            entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Name).HasMaxLength(50);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .HasConstraintName("FK__Employee__Depart__398D8EEE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
